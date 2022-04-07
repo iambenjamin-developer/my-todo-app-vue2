a97d2c6642102703e9eb633ce315a7a6f1987bd0
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-04-07 14:19:21 -0300

    vue add vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -10,5 +11,6 @@ new Vue({
   vuetify,
   VueAxios,
   axios,
+  store,
   render: h => h(App)
 }).$mount('#app')
