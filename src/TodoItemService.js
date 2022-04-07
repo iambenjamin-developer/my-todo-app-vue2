e14dd7b057f9fe5551243007119fed3be8f46127
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-04-07 13:12:42 -0300

    put

--- src/TodoItemService.js
+++ src/TodoItemService.js
@@ -7,30 +7,50 @@ class TodoItemService {
     getAllTodoItems() {
         return axios.get(API_BASE_URL);
     }
-/*
-    addToDoItem(todoItemName) {
-
-        let body = {
-            name: toDoItemName
+    /*
+        addToDoItem(todoItemName) {
+    
+            let body = {
+                name: toDoItemName
+            }
+            
+            axios.post(API_BASE_URL, body)
+                .then(response => {
+    
+                    return response.status;
+                })
         }
-        
-        axios.post(API_BASE_URL, body)
-            .then(response => {
-
-                return response.status;
-            })
-    }
-*/
+    */
     deleteTodoItem(todoItemId) {
         return axios.delete(API_BASE_URL + '/' + todoItemId);
     }
 
-    updateTodoItem(todoItem, todoItemId) {
-        return axios.put(API_BASE_URL + '/' + todoItemId, todoItem);
+    markToDoItemAsDone(toDoItemId) {
+
+        let body = {
+            markAsCompleted: true
+        }
+
+        axios.put(`${API_BASE_URL}/${toDoItemId}`, body)
+            .then(response => {
+
+                const statusCode = response.status;
+                console.log(`ToDoItemId:'${toDoItemId}' has been marked as done - Status Code:${statusCode}`);
+            })
     }
 
-    updateTodoItem2(todoItem, todoItemId) {
-        return axios.put(API_BASE_URL + '/' + todoItemId, todoItem);
+    markToDoItemAsIncompleted(toDoItemId) {
+
+        let body = {
+            markAsCompleted: false
+        }
+
+        axios.put(`${API_BASE_URL}/${toDoItemId}`, body)
+            .then(response => {
+
+                const statusCode = response.status;
+                console.log(`ToDoItemId:'${toDoItemId}' has been marked as done - Status Code:${statusCode}`);
+            })
     }
 
 
